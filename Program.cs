2ac037efe5e4745f9d34129d47d296f79c23a3a9
Jordan <jordan.d.carlton@gmail.com>
2021-09-28 17:53:35 +0100

    added regex matches method

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+namespace EmailExtraction2
+{
+    class Program
+        {
+        static void Main(string[] args)
+        {
+            string sampleString = File.ReadAllText(@"C:\Training\EmailExtraction2\sample.txt");
+            var emailRegex = new Regex(@"\w@softwire.com\s+");
+            var matches = emailRegex.Matches(sampleString);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+
+        }
+
+    }
+}
