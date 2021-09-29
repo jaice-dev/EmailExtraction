e9054258733e75cdcab64c9a01a34244af6f822d
Jordan <jordan.d.carlton@gmail.com>
2021-09-29 09:34:27 +0100

    added dictionary count for all email domains

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
+using System.Xml.XPath;
 
 namespace EmailExtraction2
 {
@@ -11,16 +13,36 @@ namespace EmailExtraction2
         static void Main(string[] args)
         {
             string inputText = File.ReadAllText(@"C:\Training\EmailExtraction2\sample.txt");
-            var emailDomainRegex = new Regex(@"\w(@softwire.com)\s+");
-            var matches = emailDomainRegex.Matches(inputText);
+            var softwireEmailRegex = new Regex(@"\w(@softwire.com)\s+");
+            var softwireMatches = softwireEmailRegex.Matches(inputText);
             int emailCount = 0;
 
-            foreach (Match match in matches)
+            foreach (Match match in softwireMatches)
             {
                 emailCount++;
             }
             
             Console.WriteLine($"Number of matches containing @softwire.com: {emailCount}");
+
+            Dictionary<string, int> domainDict = new Dictionary<string, int>();
+
+            var allEmailsRegex = new Regex(@"@[a-zA-Z0-9-_.]+");
+            var allEmailMatches = allEmailsRegex.Matches(inputText);
+
+            foreach (Match match in allEmailMatches)
+            {
+                if (!domainDict.ContainsKey(match.Value))
+                {
+                    var domainRegex = new Regex(match.Value);
+                    domainDict[match.Value] = domainRegex.Matches(inputText).Count;
+                }
+            }
+
+            foreach (var item in domainDict)
+            {
+                Console.WriteLine($"Domain: {item.Key} Frequency: {item.Value} ");
+            }
+
         }
 
     }
