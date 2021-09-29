060ed387313c0d5331c0cb836df46e996c9c6a95
Jordan <jordan.d.carlton@gmail.com>
2021-09-29 09:50:58 +0100

    added comments for readabilty

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ namespace EmailExtraction2
         {
         static void Main(string[] args)
         {
+            // First Excercise
+            
+            /*
             string inputText = File.ReadAllText(@"C:\Training\EmailExtraction2\sample.txt");
             var softwireEmailRegex = new Regex(@"\w(@softwire.com)\s+");
             var softwireMatches = softwireEmailRegex.Matches(inputText);
@@ -23,21 +26,26 @@ namespace EmailExtraction2
             }
             
             Console.WriteLine($"Number of matches containing @softwire.com: {emailCount}");
-
+            */
+            
+            // Initialise dictionary of email domains
             Dictionary<string, int> domainDict = new Dictionary<string, int>();
 
             var allEmailsRegex = new Regex(@"[a-zA-Z0-9-_.]+(@[a-zA-Z0-9-_.]+)\s+");
             var allEmailMatches = allEmailsRegex.Matches(inputText);
-
+            
+            // Loop over each match, and add to dictionary if new with value of a
+            // new Matches class count for the new domain
             foreach (Match match in allEmailMatches)
             {
-                if (!domainDict.ContainsKey(match.Groups[1].Value))
+                if (!domainDict.ContainsKey(match.Groups[1].Value)) // Groups[1] gets 1st regex group - bit in ()
                 {
                     var domainRegex = new Regex(@$"{match.Groups[1].Value}\s+");
                     domainDict[match.Groups[1].Value] = domainRegex.Matches(inputText).Count;
                 }
             }
-
+            
+            // Loop through dictionary and write contents to console
             foreach (var item in domainDict)
             {
                 Console.WriteLine($"Domain: {item.Key} Frequency: {item.Value} ");
