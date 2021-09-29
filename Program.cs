61d36ac74052c437ccfa232521ff53bc2760e33f
Jordan <jordan.d.carlton@gmail.com>
2021-09-29 13:24:59 +0100

    added ordered dict and user frequency limit

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using System.Xml.XPath;
@@ -12,26 +13,28 @@ namespace EmailExtraction2
         {
         static void Main(string[] args)
         {
+            var inputText = File.ReadAllText(@"C:\Training\EmailExtraction2\sample.txt");
+             
             // First Excercise
-            
-            /*
-            string inputText = File.ReadAllText(@"C:\Training\EmailExtraction2\sample.txt");
+             /*
             var softwireEmailRegex = new Regex(@"\w(@softwire.com)\s+");
             var softwireMatches = softwireEmailRegex.Matches(inputText);
             int emailCount = 0;
 
-            foreach (Match match in softwireMatches)
-            {
-                emailCount++;
-            }
-            
-            Console.WriteLine($"Number of matches containing @softwire.com: {emailCount}");
-            */
-            
-            // Initialise dictionary of email domains
+             foreach (Match match in softwireMatches)
+             {
+                 emailCount++;
+             }
+             
+             Console.WriteLine($"Number of matches containing @softwire.com: {emailCount}");
+             */
+             
+             // Initialise dictionary of email domains
+             
             Dictionary<string, int> domainDict = new Dictionary<string, int>();
 
-            var allEmailsRegex = new Regex(@"[a-zA-Z0-9-_.]+(@[a-zA-Z0-9-_.]+)\s+");
+            var regexString = @"[a-zA-Z0-9-_.]+(@[a-zA-Z0-9-_.]+)\s+";
+            var allEmailsRegex = new Regex(regexString);
             var allEmailMatches = allEmailsRegex.Matches(inputText);
             
             // Loop over each match, and add to dictionary if new with value of a
@@ -46,10 +49,23 @@ namespace EmailExtraction2
             }
             
             // Loop through dictionary and write contents to console
-            foreach (var item in domainDict)
+            var counter = 0;
+            var maxDomainsNum = 10;
+            var minFrequency = 1;
+            
+            foreach (var item in domainDict.OrderBy(key => key.Value).Reverse())
             {
+                if (counter >= maxDomainsNum || item.Value <= minFrequency)
+                {
+                    break;
+                }
+                
                 Console.WriteLine($"Domain: {item.Key} Frequency: {item.Value} ");
+                counter++;
             }
+            
+            // Phone Number Validation
+            // (?:\+\d\d\s?)|0[0-9]{4}\s?[0-9]{6}
 
         }
 
